faf9fa22845b1eab9a4e4f0312c115b9770ac5a0
Manav Rupani <mkvmanav@gmail.com>
2024-10-27 13:47:37 -0400

    day1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Hero from './pages/hero'
+import './static/css/App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Hero.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-screen bg-gray-100 '>
+            <div className='flex flex-col items-left justify-end h-full w-full pb-10 pl-10'>
+                
+                <span className='uppercase text-[60px] pl-5'>Manav rupani</span>
+                <span className='text-[20px] font-thin mt-[-20px] pl-6'>Full-Stack Developer</span>
+                
+            </div>
+      </div>
+  )
+}
+
+export default Hero
